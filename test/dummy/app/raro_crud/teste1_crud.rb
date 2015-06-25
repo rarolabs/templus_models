2f9b6abb5bb79a2ccf90f0c978ceb2419fb29dac
Leonardo Herbert <leonardo@rarolabs.com.br>
2015-06-25 18:34:25 -0300

    fix sort field

--- test/dummy/app/raro_crud/teste1_crud.rb
+++ test/dummy/app/raro_crud/teste1_crud.rb
@@ -3,7 +3,7 @@ class Teste1Crud < RaroCrud
   titulo "Teste1"
   link_superior "Novo Teste1", id: "novo-button", icon: "plus", link: "new"
 
-  campo_tabela :teste,  label: "Nome"
+  campo_tabela :teste,  label: "Nome", sort_field: :teste_nome
   
   grupo_formulario :teste, [
     {campo: :nome, label: "Nome"}

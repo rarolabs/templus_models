f4f0dd4e619ed849c6be060dbdb933d5af237b9d
Diego Lima <lima@rarolabs.com.br>
2015-07-06 15:10:46 -0300

    [fix] form groups class name controller

--- app/controllers/crud_controller.rb
+++ app/controllers/crud_controller.rb
@@ -163,7 +163,7 @@ class CrudController < ApplicationController
       chave = "#{key}_attributes"
       group = {chave => [:id, :_destroy]}
       groups.each do |field|
-        modelo = key.to_s.singularize.camelcase.constantize
+        modelo = @model.reflect_on_association(key.to_s).class_name.constantize
         if modelo.reflect_on_association(field[:attribute])
           if modelo.reflect_on_association(field[:attribute]).macro == :belongs_to
             group[chave] << "#{field[:attribute]}_id".to_sym
--- lib/templus_models/version.rb
+++ lib/templus_models/version.rb
@@ -1,3 +1,3 @@
 module TemplusModels
-  VERSION = "1.2.4"
+  VERSION = "1.2.5"
 end
